646d0ad3a1ee7d5da0028f0d94eada19c4ee4a0c
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2023-10-01 13:07:04 +0300

    add hide button for news

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
   standalone: true,
-  imports: [MatIconModule]
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  animations: [
+    trigger('showNews', [
+      state('opened', style({
+        transition: 'init',
+      })),
+      state('closed', style({
+        transform: 'translate(100%, 0)'
+      })),
+      transition('opened <=> closed', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  isNewsShown = true;
+}
